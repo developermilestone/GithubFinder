827e8ace0d73f51f4674fba7e82da7a97a8c4941
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-01 22:50:35 +0000

    feat(state-manager): added store provider to the main wrapper

--- src/layouts/wrapper.tsx
+++ src/layouts/wrapper.tsx
@@ -1,4 +1,5 @@
 "use client";
+import StoreProvider from "@/providers/StoreProvider";
 import ThemeProvider from "@/providers/ThemeProvider";
 import '@/theme/globals.css';
 import { type FC, type ReactNode } from "react";
@@ -10,8 +11,10 @@ export const LayoutWrapper: FC<LayoutProps> = (props: LayoutProps) => {
   const { children } = props;
   
   return (
+    <StoreProvider>
       <ThemeProvider>
           {children}
       </ThemeProvider>
+    </StoreProvider>
   )
 };
