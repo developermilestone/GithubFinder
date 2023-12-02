018288d67011fbe1e369a83e91c21438792269f1
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-02 12:55:51 +0000

    fix(auth): changed the return path for auth process

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -3,12 +3,12 @@ import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
     login: handleLogin({
-        returnTo: '/main',
+        returnTo: '/',
     }),
     signup: handleLogin({
         authorizationParams: {
             screen_hint: 'signup',
         },
-        returnTo: '/main',
+        returnTo: '/',
     })
 });
