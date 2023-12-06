338b793a717a5ec5c5165e2d6a6c39ce5086dce8
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-06 19:29:44 +0000

    feat: added splashscreen to handle with initial loading

--- src/components/SplashScreen/component.tsx
+++ src/components/SplashScreen/component.tsx
@@ -1,32 +1,28 @@
-import { Box } from '@mui/material';
+"use client";
+import { Box, LinearProgress, styled } from '@mui/material';
 import type { FC } from 'react';
+import { Logo } from '..';
 
+const SplashScreenWrapper = styled(Box)({
+    alignItems: 'center',
+    backgroundColor: 'background.paper',
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    justifyContent: 'center',
+    left: 0,
+    p: 3,
+    position: 'fixed',
+    top: 0,
+    width: '100vw',
+    zIndex: 1400,
+});
 
 export const SplashScreen: FC = () => (
-    <Box
-        sx={{
-            alignItems: 'center',
-            backgroundColor: 'background.paper',
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100vh',
-            justifyContent: 'center',
-            left: 0,
-            p: 3,
-            position: 'fixed',
-            top: 0,
-            width: '100vw',
-            zIndex: 1400,
-        }}
-    >
-        <Box
-            sx={{
-                display: 'inline-flex',
-                height: 48,
-                width: 48,
-            }}
-        >
-            Github Finder
+    <SplashScreenWrapper>
+        <Logo clickable={false} />
+        <Box sx={{ width: '100%' }}>
+         <LinearProgress />
         </Box>
-    </Box>
+    </SplashScreenWrapper>
 );
