1014c23bf0c308dd456f851a98efe6763adc7b92
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-06 19:22:49 +0000

    feat: added favourite store

--- src/store/slices/favoritesSlice.ts
+++ src/store/slices/favoritesSlice.ts
@@ -1,22 +1,31 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface Favorite {
-  id: string;
+type Language = {
+  name: string;
+  color?: string;
+};
+interface Repository {
+  name?: string;
+  url?: string;
+  rating?: number;
+  id?: string;
+  description?: string;
+  languages?: Language[];
 }
 
-type FavoritesState = Favorite[];
+type FavoritesState = Repository[];
 
 export const favoritesSlice = createSlice({
   name: 'favorites',
   initialState: [] as FavoritesState,
   reducers: {
-    addFavorite: (state, action: PayloadAction<Favorite>) => {
+    addFavorite: (state, action: PayloadAction<Repository>) => {
       const duplicate = state.find(repo => repo.id === action.payload.id);
       if (!duplicate) {
         state.push(action.payload);
       }
     },
-    removeFavorite: (state, action: PayloadAction<{ id: string }>) => {
+    removeFavorite: (state, action: PayloadAction<Repository>) => {
       return state.filter(repo => repo.id !== action.payload.id);
     },
     addListFavorite: (state, action: PayloadAction<FavoritesState>) => {
