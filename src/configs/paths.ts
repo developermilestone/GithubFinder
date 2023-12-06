348fbf93707a4126d0f9be6e13ec2967a88b235d
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-06 19:18:56 +0000

    feat: added path configuration

--- src/configs/paths.ts
+++ src/configs/paths.ts
@@ -2,4 +2,29 @@ const externalPath = {
     githubApi: 'https://api.github.com/graphql',
 }
 
-export { externalPath }
+const paths = {
+    main: '/',
+    favorites: '/favorites',
+    api:{
+        auth:{
+            login: '/api/auth/login',
+            logout: '/api/auth/logout',
+            callback: '/api/auth/callback',
+        },
+        graphql: '/api/graphql',
+    }
+}
+
+
+const menu = [
+    {
+        session: 'home',
+        path: paths.main,
+    },
+    {
+        session: 'favorites',
+        path: paths.favorites,
+    }
+]
+export { externalPath, menu, paths }
+
