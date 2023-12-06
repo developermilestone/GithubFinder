c0484d031bd385e3e165ebb39e0bc75f7959a121
Bruno Pereira <b.alexadre.p@outlook.com>
2023-12-06 19:21:51 +0000

    refactor: theme handler

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,32 +1,39 @@
+import { purple } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 const defaultTheme = createTheme({
-  palette: {
-    mode: 'light',
-    background: {
-        default: '#fff'
+    palette: {
+        mode: 'light',
+        background: {
+            default: '#fff',
+        },
+        primary: {
+            main: purple[500],
+        },
+        secondary: {
+            main: '#f44336',
+        },
     },
-    
-  },
-  typography: {
-    fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
-    fontWeightLight: 300,
-    fontWeightRegular: 400,
-    fontWeightMedium: 600,
-    fontWeightBold: 700,
-    fontSize: 10,
-},
-  components: {
-    MuiAlert: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          ...(ownerState.severity === 'info' && {
-            backgroundColor: '#60a5fa',
-          }),
-        }),
-      },
+    typography: {
+        fontFamily:
+            'Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+        fontWeightLight: 300,
+        fontWeightRegular: 400,
+        fontWeightMedium: 600,
+        fontWeightBold: 700,
+        fontSize: 10,
     },
-  },
+    components: {
+        MuiAlert: {
+            styleOverrides: {
+                root: ({ ownerState }) => ({
+                    ...(ownerState.severity === 'info' && {
+                        backgroundColor: '#60a5fa',
+                    }),
+                }),
+            },
+        },
+    },
 });
 
 export default defaultTheme;
